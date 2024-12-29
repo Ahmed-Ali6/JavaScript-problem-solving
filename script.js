087a3dbaba1fe9f35b899e536263df3cd6345d97
Ahmed Ali <ahmedali007b@gmail.com>
2024-12-29 21:05:30 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,95 @@
+// 1 - Write a JavaScript program to calculate the sum of 2 number
+
+// var num1 = +prompt("Enter number 1");
+// var num2 = +prompt("Enter number 2");
+
+// console.log("sum of", num1, "+", num2, "=", num1+num2);
+
+
+// **************************************************************
+// 2-Write a JavaScript program to check number if even or odd
+
+// var num = +prompt("Enter a number")
+// if(num!=0){
+//     if(num%2==0){
+//         console.log("Even");
+//     }
+//     else{
+
+//         console.log("odd");
+
+//     }
+// }
+
+// *************************************************************************
+
+// 3- Write a JavaScript program that takes a month and returns the number of days in that month.
+
+// var month = prompt("Enter a name of a month ");
+
+// switch (month){
+//     case "January":
+//     case "March":
+//     case "May":
+//     case "July":
+//     case "August":
+//     case "October":
+//     case "December":
+//         console.log(month, ": is 31 days.")
+//         break;
+//     case "April":
+//     case "June":
+//     case "September":
+//     case "November":
+//         console.log(month, ": is 30 days.")
+//         break;
+//     case "February":
+//         console.log(month, ": is 28 or 29 days.")
+//         break;
+//     default:
+//         console.log("You have entered an incorrect word.");
+
+    
+// }
+
+
+
+// **********************************************************************
+
+// 4 - Write a JavaScript program to find max number between 2 number
+
+// var num1 = +prompt("Enter number 1");
+// var num2 = +prompt("Enter number 2");
+
+// if (num1>num2){
+//     console.log(num1, ": is bigger");
+// }
+// else if(num1<num2){
+//     console.log(num2, ": is bigger");
+// }
+// else{
+//     console.log("They're equal");
+// }
+// **********************************************************************
+
+// 5- Write  a JavaScript program to check if character vowel or constant
+
+// var letter = prompt("Enter a letter");
+
+// switch (letter) {
+//     case 'a':
+//     case 'A':
+//     case 'e':
+//     case 'E':
+//     case 'i':
+//     case 'I':
+//     case 'o':
+//     case 'O':
+//     case 'u':
+//     case 'U':
+//         console.log(letter, ": is vowel")
+//         break;
+//     default:
+//         console.log(letter, ": is constant")
+
+// }
